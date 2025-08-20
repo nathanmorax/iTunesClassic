b2d96aafb728ab9824fcada629f61edd2ea6cf21
Jonathan Jesus <nathanmorax@outlook.com>
2025-08-19 20:23:30 -0600

    Initial commit

---
+++ iTunesClassic/iTunesClassicApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesClassicApp.swift
+//  iTunesClassic
+//
+//  Created by Jesus Mora on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct iTunesClassicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
